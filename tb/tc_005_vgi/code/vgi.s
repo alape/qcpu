94820eeced46113853d06b331c859588c78f2ff6
Algirdas Lapė <eifscf@gmail.com>
2026-02-11 16:21:32 +0300

    tb: rewrote tc_005's assembly using LLR's textmode as base

--- tb/tc_005_vgi/code/vgi.s
+++ tb/tc_005_vgi/code/vgi.s
@@ -1,28 +1,50 @@
 .text
     _start:     ld sc, $stack
-                ld r0, 0x403        ; VGI framebuffer base address
+                ld r7, 0x403        ; VGI framebuffer base address
 
-                ld r1, 0x4b         ; 0x4b << 12 + 0x400 = \
-                lsh r1, 12          ;   640 * 480 + FB_BASE_ADDRESS
-                or r1, r1, 0x400
+                ld r2, 0xFFFF       ; set r2 (pixel colour) to 0xFFFFFF (100% white)
+                lsh r2, 16
+                or r2, r2, 0xFF00
 
-                ld r2, $putpixel    ; loop vector
+                ld r0, 5
+                ld r1, 5
+                jal putpixel
 
-                ld r3, 0            ; current pixel colour
-                ld r4, 0            ; pixel shift scratchpad
+                ld r0, 15
+                jal putpixel
 
-    putpixel:   ;st ones, r0         ; fill two pixels at a time to create a checkerboard pattern
-                ;add r0, r0, 1
-                ;st zeroes, r0
-                ;add r0, r0, 1
+                ld r1, 15
+                jal putpixel
 
-                add r4, r3, 0
-                lsh r4, 8
-                st r4, r0
-                add r3, r3, 0x36
-                add r0, r0, 1
+                ld r0, 5
+                jal putpixel
 
-                blt r2, r0, r1      ; loop until all pixels are filled
+                jmp endloop
+
+
+    putpixel:   ; update a pixel by specified coordinates in the VGI framebuffer:
+                ;   r0: X coordinate,
+                ;   r1: Y coordinate,
+                ;   r2: colour (24bpp big-endian BGR)
+                
+                ; calculate pixel's linear coordinates (i.e. framebuffer offset):
+                add r3, r7, r0          ; addr = base_addr + X
+
+                ld r4, 0                ; X coordinate counter
+                ld r5, $_ycalc          ; _ycalc() vector
+                ld r6, $_wrpixel        ; _wrpixel() vector
+
+                beq r6, r1, 0           ; if Y = 0, skip _ycalc()
+
+                _ycalc:
+                    add r3, r3, 0x280  ; addr = addr + 640 * X
+                    add r4, r4, 1
+                    blt r5, r4, r1
+
+                _wrpixel:
+                    st r2, r3          ; write pixel to the framebuffer
+
+                ret 
 
     endloop:    nop                 ; when framebuffer is filled, enter endless loop
                 jmp endloop
